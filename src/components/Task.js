f0f4cd8f2ac4ebcc2697742978f8d3551f4110f4
Igor Usovich <usovigorok@gmail.com>
2023-03-18 20:59:49 +0100

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,3 @@
-import React from "react";
-import PropTypes from "prop-types";
-
 export default function Task({
   task: { id, title, state },
   onArchiveTask,
@@ -30,6 +27,7 @@ export default function Task({
           readOnly={true}
           name="title"
           placeholder="Input title"
+          style={{ background: "red" }}
         />
       </label>
 
@@ -47,19 +45,3 @@ export default function Task({
     </div>
   );
 }
-
-Task.propTypes = {
-  /** Composition of the task */
-  task: PropTypes.shape({
-    /** Id of the task */
-    id: PropTypes.string.isRequired,
-    /** Title of the task */
-    title: PropTypes.string.isRequired,
-    /** Current state of the task */
-    state: PropTypes.string.isRequired,
-  }),
-  /** Event to change the task to archived */
-  onArchiveTask: PropTypes.func,
-  /** Event to change the task to pinned */
-  onPinTask: PropTypes.func,
-};
